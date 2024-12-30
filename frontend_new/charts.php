f0ae97a4b2e72a8b7a0c14da75c41f5f48f0d11d
AndreaOstruszkova <andrea.ostruszkova@outlook.com>
2024-12-30 11:15:54 +0100

    Added smooth scroll

--- frontend_new/charts.php
+++ frontend_new/charts.php
@@ -189,4 +189,25 @@
     </article>
 </div>
 
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const links = document.querySelectorAll('a[href^="#"]');
+
+        for (const link of links) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+
+                if (targetElement) {
+                    window.scrollTo({
+                        top: targetElement.offsetTop,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        }
+    });
+</script>
+
 <?php include 'footer.php'; ?>
