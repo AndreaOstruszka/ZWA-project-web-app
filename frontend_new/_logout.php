783ccbe0d5760571e72de3c56cb98378ab870e6b
AndreaOstruszkova <andrea.ostruszkova@outlook.com>
2025-01-02 21:42:29 +0100

    Changed DB connection from msqli to PDO, added logout function

---
+++ frontend_new/_logout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+function logoutUser() {
+    // Clear session and cookie data, then redirect to login page or show an error
+    try {
+        session_unset();
+        session_destroy();
+    } catch (Throwable $th) {
+        echo $th;
+    }
+    //setcookie('user_token', '', time() - 3600);  // Delete the cookie
+    header("Location: index.php");
+    exit();
+}
+
+logoutUser();
